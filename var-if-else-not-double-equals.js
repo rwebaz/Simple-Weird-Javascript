f8f03acae569f8e9efea58fe528ef2d60841e533
Robert Weber <rwebaz@me.com>
2017-05-13 18:44:11 -0700

    Rename js
    Move functions from main Weird js Tutorial

---
+++ var-if-else-not-double-equals.js
@@ -0,0 +1,21 @@
+// Weird-js Tutorial: Var If Else Not Double Equals
+var c;
+c = "Hello There!";
+
+function a() {
+ "use strict";
+ console.log("The function named 'a' has been called!");
+}
+
+function b() {
+ "use strict";
+ if (c !== undefined) {
+  console.log("The variable 'c' is de-fined!");
+ } else {
+  console.log("The variable 'c' is un-de-fined!");
+ }
+}
+
+a();
+b();
+console.log(c);
