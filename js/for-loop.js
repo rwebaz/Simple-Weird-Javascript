3370fd0b3cfd1e62f966f22cbe1a04ca1f42df57
Robert Weber <rwebaz@me.com>
2017-05-20 14:16:34 -0700

    Add For Loop exercise
    Javascript: The Weird Parts

---
+++ js/for-loop.js
@@ -0,0 +1,16 @@
+// For Loop
+
+// Declare the function name
+var a;
+
+function a() {
+	"use strict";
+	var i;
+	
+	for (i = 0; i < 10; i = i + 1) {
+		// console.log(i);
+		console.log(`The computer just performed iteration number ${i}.`);
+	}
+}
+
+a();
